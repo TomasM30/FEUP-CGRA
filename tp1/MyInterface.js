b7db49206078df28325199688c1fa9c0c89c767b
José Martins <up202108794@edu.fe.up.pt>
2024-02-18 23:37:11 +0000

    finished code for tp1. Still need to add screenshots and update readme

--- tp1/MyInterface.js
+++ tp1/MyInterface.js
@@ -23,6 +23,21 @@ export class MyInterface extends CGFinterface {
         //Slider element in GUI
         this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
 
+        //Checkbox element to display Diamond
+        this.gui.add(this.scene, 'displayDiamond').name('Display Diamond');
+
+        //Checkbox element to display Triangle
+        this.gui.add(this.scene, 'displayTriangle').name('Display Triangle');
+
+        //Checkbox element to display Parallelogram
+        this.gui.add(this.scene, 'displayParallelogram').name('Display Parallelogram');
+
+        //Checkbox element to display Small Triangle
+        this.gui.add(this.scene, 'displayTriangleSmall').name('Display Small Triangle');
+
+        //Checkbox element to display Big Triangle
+        this.gui.add(this.scene, 'displayTriangleBig').name('Display Big Triangle');
+
         return true;
     }
 }
